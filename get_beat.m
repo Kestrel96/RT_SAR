15d9b627857bc440b2d4dd7048961458b1c1f469
Kestrel <kuba.poltorak@gmail.com>
2023-01-07 18:17:40 +0100

    wip

--- get_beat.m
+++ get_beat.m
@@ -5,6 +5,7 @@ function beat = get_beat(r,t,lambda,Beta,T)
     c=3e8;
  
  phi=4*pi*r/lambda; % phase of IF signal
+ r=10;
  f_if=Beta*2*r/(T*c); % frequency of IF signal
  % r=f_if*T*c/(2*Beta)
  beat=exp(i*(2*pi*f_if*t+phi));
